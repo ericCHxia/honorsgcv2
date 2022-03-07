65b7db432f62981cc7924fa13d939a89e8f29c8f
EricXia <xch@raxch.cn>
2022-03-07 09:38:40 +0800

    创建 Article的 Dto类 ArticleSimple

---
+++ src/main/java/cn/honorsgc/honorv2/article/dto/ArticleSimple.java
@@ -0,0 +1,23 @@
+package cn.honorsgc.honorv2.article.dto;
+
+import cn.honorsgc.honorv2.user.dto.UserDto;
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class ArticleSimple implements Serializable {
+    private final Long id;
+    private final UserDto user;
+    private final String tagName;
+    @NotNull
+    private final String title;
+    private final String describe;
+    private final Integer state;
+    private final Date createTime;
+    private final Integer type;
+    @NotNull
+    private final Integer haveComment;
+}
