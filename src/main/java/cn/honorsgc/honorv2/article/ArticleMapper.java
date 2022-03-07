3839aa0c5a6a9181e81a686048c2613848c32f73
EricXia <xch@raxch.cn>
2022-03-07 09:38:01 +0800

    ArticleSimple 添加Mapper

--- src/main/java/cn/honorsgc/honorv2/article/ArticleMapper.java
+++ src/main/java/cn/honorsgc/honorv2/article/ArticleMapper.java
@@ -1,8 +1,11 @@
 package cn.honorsgc.honorv2.article;
 
 import cn.honorsgc.honorv2.article.dto.ArticleCommentDto;
+import cn.honorsgc.honorv2.article.dto.ArticleSimple;
+import cn.honorsgc.honorv2.article.enity.Article;
 import cn.honorsgc.honorv2.article.enity.ArticleComment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -10,5 +13,13 @@ import java.util.List;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface ArticleMapper {
     ArticleCommentDto articleCommentToArticleCommentDto(ArticleComment articleComment);
+
     List<ArticleCommentDto> articleCommentToArticleCommentDto(List<ArticleComment> articleComment);
+
+    @Mapping(source = "tag.name", target = "tagName")
+    @Mapping(source = "have_comment", target = "haveComment")
+    ArticleSimple articleToArticleSimple(Article article);
+
+    List<ArticleSimple> articleToArticleSimple(List<Article> article);
+
 }
